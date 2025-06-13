12a768b824aef331a54c128b8d273c69b58361d5
FaridAquino <farid.aquino@utec.edu.pe>
2025-06-13 18:10:15 -0500

    corregir errores

--- frontendHackaton3Practica/src/App.tsx
+++ frontendHackaton3Practica/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from './Pages/Login'
+import Info from './Pages/Info'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
---
+++ frontendHackaton3Practica/src/pages/login.tsx
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
+function Login(){
+    return(
+        <></>
+    );
+}
+
+export default Login;
+
